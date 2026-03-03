3919d5fba6daedb5ac1e643f10e0572139ae390c
qubicfire <burpilav@gmail.com>
2026-03-03 23:29:36 +0300

    uac added

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,3 +1,11 @@
 fn main() {
-    tauri_build::build();
+    let mut windows = tauri_build::WindowsAttributes::new();
+    let manifest = std::fs::read_to_string("app.manifest")
+        .expect("Failed to read app.manifest");
+    
+    windows = windows.app_manifest(manifest);
+
+    tauri_build::try_build(
+        tauri_build::Attributes::new().windows_attributes(windows)
+    ).expect("failed to run build script");
 }
